5c16a4d4dc8404d8a4da1a5f4b1ec46608381646
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2024-12-07 02:40:47 +0000

    add cv/resume

--- src/navigation.config.ts
+++ src/navigation.config.ts
@@ -3,5 +3,5 @@ import type { LinkProps } from "@components/Link.astro";
 export const navigation: LinkProps[] = [
     { label: 'Home', target: '/' },
     { label: 'Projects', target: '/projects' },
-    { label: 'CV', target: '/cv' }
+    { label: 'CV / Résumé', target: '/cv' }
 ];
